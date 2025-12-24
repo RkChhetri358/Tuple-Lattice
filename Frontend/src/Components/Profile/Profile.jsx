9a5fc63838830b36d8434134027425ed96b72e00
proatease <61pratisthitbaral@gmail.com>
2025-12-24 23:09:57 +0545

    profile balance and img

--- Frontend/src/Components/Profile/Profile.jsx
+++ Frontend/src/Components/Profile/Profile.jsx
@@ -1,18 +1,19 @@
 import "./Profile.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWallet } from "@fortawesome/free-solid-svg-icons";
 export default function Profile() {
   return (
     <div className="profile-container">
-
       <div className="profile-grid">
-
         {/* LEFT COLUMN */}
         <div className="left-col">
-
           <div className="balance-card">
             <div className="balance-top">
               <span>Total Balance</span>
-              <span className="wallet-icon">💳</span>
+              <span className="wallet-icon">
+                {" "}
+                <FontAwesomeIcon icon={faWallet} />
+              </span>
             </div>
 
             <h1 className="balance-amount">Rs 13,000.67</h1>
@@ -20,20 +21,18 @@ export default function Profile() {
           </div>
 
           <div className="chart-box" />
-
         </div>
 
         {/* RIGHT COLUMN */}
         <div className="right-col">
-
           <div className="profile-row">
-            <img src="/profile.jpg" alt="artist" />
+            <img src="/profile.png" className="profile-img" alt="artist" />
             <div className="profile-text">
               <p>
-                “I share my work here with clear ownership, fair value,
-                and respect for the creative process.”
+                “I share my work here with clear ownership, fair value, and
+                respect for the creative process.”
               </p>
-              <h4>Suresh Gupa</h4>
+              <h3>Suresh Gupa</h3>
               <span>verified artist</span>
             </div>
           </div>
@@ -43,25 +42,35 @@ export default function Profile() {
 
             <div className="rev-item">
               <span>Distributor</span>
-              <div className="bar"><div style={{ width: "60%" }} /></div>
-              <span className="right">60% <br /> Rs. 12,000</span>
+              <div className="bar">
+                <div style={{ width: "60%" }} />
+              </div>
+              <span className="right">
+                60% <br /> Rs. 12,000
+              </span>
             </div>
 
             <div className="rev-item">
               <span>Collaborator</span>
-              <div className="bar"><div style={{ width: "30%" }} /></div>
-              <span className="right">30% <br /> Rs. 3,000</span>
+              <div className="bar">
+                <div style={{ width: "30%" }} />
+              </div>
+              <span className="right">
+                30% <br /> Rs. 3,000
+              </span>
             </div>
 
             <div className="rev-item">
               <span>Platform</span>
-              <div className="bar"><div style={{ width: "10%" }} /></div>
-              <span className="right">10% <br /> Rs. 1,000</span>
+              <div className="bar">
+                <div style={{ width: "10%" }} />
+              </div>
+              <span className="right">
+                10% <br /> Rs. 1,000
+              </span>
             </div>
           </div>
-
         </div>
-
       </div>
     </div>
   );
